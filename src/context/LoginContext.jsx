eb39079acb904ba0bc3d38f3211ace36f539e18e
Aytan Khaligli <ayten.xaliqli@gmail.com>
2023-11-03 13:24:49 +0400

    add login functionalty

--- src/context/LoginContext.jsx
+++ src/context/LoginContext.jsx
@@ -5,26 +5,42 @@ const LoginContext = createContext();
 const BASE_URL =
   "https://api-explorer.alfresco.com/alfresco/api/-default-/public/alfresco/versions/1";
 
+const customAxios = axios.create({
+  baseURL: "https://1curd3ms.trials.alfresco.com", // Replace with your API server's URL
+  headers: {
+    "Access-Control-Allow-Origin": "*", // Replace with your allowed origins
+    "Access-Control-Allow-Methods": "*", // Replace with your allowed methods
+    "Access-Control-Allow-Headers": "*", // Replace with your allowed headers
+    "Access-Control-Expose-Headers": "*", // Replace with your exposed headers
+  },
+});
+
 function LoginContextProvider({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // const logout = () => {
-  //   // Perform logout actions (e.g., clearing cookies or tokens)
-  //   setUser(null);
-  //   setIsAuthenticated(false);
-  // };
-
   const signIn = async (username, password) => {
-    setIsLoggedIn(true);
     try {
-      const response = await axios.get(`${BASE_URL}/audit-applications`);
-      console.log("hello");
-      console.log(response);
+      const base64Credentials = btoa(`${username}:${password}`);
+
+      const response = await customAxios.post(
+        "/alfresco/api/-default-/public/authentication/versions/1/tickets",
+        { userId: username, password: password },
+        {
+          headers: {
+            Authorization: `Basic ${base64Credentials}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log("Request successful");
+      setIsLoggedIn(true);
     } catch (error) {
-      console.log("hello");
-      console.log(error);
+      console.log("Request failed");
+      console.error(error);
     }
   };
+
   return (
     <LoginContext.Provider value={{ isLoggedIn, signIn }}>
       {children}
